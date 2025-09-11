426c6724ba70c0506d3ac02364947389f6d6ab24
go69bah <eva.stankova@tum.de>
2025-09-11 21:58:48 +0200

    fixed functionality of tailwind css --> it worksssgit add .!

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
 export default function HomePage() {
   return (
-    <main className="p-6">
-      <h1 className="text-3xl font-bold">Hello TS + Next + Tailwind</h1>
-      <p className="mt-2">If this is big and bold, Tailwind works.</p>
+    <main className="min-h-screen p-8 grid place-items-center">
+      <div className="max-w-xl w-full rounded-2xl shadow p-6">
+        <h1 className="text-3xl font-bold">Hello TS + Next + Tailwind</h1>
+        <p className="mt-2 text-sm text-gray-600">
+          If this heading is big and bold, Tailwind works.
+        </p>
+
+        <div className="mt-8 w-40 h-40 bg-blue-500 rounded-lg text-white grid place-items-center">
+          Blue
+        </div>
+
+      </div>
     </main>
   );
 }
